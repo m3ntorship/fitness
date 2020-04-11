af46d3e7013b4fe1b53cedcc9f4bb62ed8e52353
zahraa333 <zahraa.khalil2020@gmail.com>
2020-04-11 17:49:35 +0200

    added header, banner & navigation

---
+++ header-banner-navigation/app.js
@@ -0,0 +1,10 @@
+
+const nav = document.getElementById('nav');
+
+// window.addEventListener('scroll', function () {
+//     nav.style.backgroundColor = "#121212";
+//     console.log(nav);
+// })
+
+
+
